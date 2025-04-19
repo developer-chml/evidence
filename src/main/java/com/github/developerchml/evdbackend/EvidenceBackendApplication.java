2ee11e972927eac96fd7d5eb5ceafda83f2e9a44
CHML <developerchml@gmail.com>
2025-04-19 10:56:59 -0300

    Create project

---
+++ src/main/java/com/github/developerchml/evdbackend/EvidenceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.developerchml.evdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvidenceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/developerchml/evdbackend/EvidenceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.developerchml.evdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvidenceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
