8589c0fe69c1fd0093115bacca88b32afd120e1c
CHML <developerchml@gmail.com>
2025-04-22 23:39:47 -0300

    Implementa Enum UserRole e UserStatus
    UserRole - Representa os papeis e retrições de acesso para o usuário.
    UserStatus - Representa os status que libera ou restrige acesso para o usuário.

---
+++ src/main/java/com/github/developerchml/evdbackend/core/entities/user/UserStatus.java
@@ -0,0 +1,21 @@
+package com.github.developerchml.evdbackend.core.entities.user;
+
+public enum UserStatus {
+    ACTIVE,
+    CREATED,
+    DELETED,
+    BLOCKED;
+
+    public static UserStatus toEnum(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        for (UserStatus s : UserStatus.values()) {
+            if (str.equalsIgnoreCase(s.name())) {
+                return s;
+            }
+        }
+        throw new RuntimeException();
+    }
+}
