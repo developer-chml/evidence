06e050cdc685d21eb3696e77f9e2ce61a8d34f38
CHML <developerchml@gmail.com>
2025-04-22 23:35:59 -0300

    Implementa valueObject Email
    Classe customizada para representar e validar o email.

---
+++ src/main/java/com/github/developerchml/evdbackend/core/entities/valueObject/Email.java
@@ -0,0 +1,43 @@
+package com.github.developerchml.evdbackend.core.entities.valueObject;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public class Email {
+    protected String value;
+
+    protected Email() {
+    }
+
+    protected Email(String value) {
+        if (!validate(value)) {
+            throw new RuntimeException("email is invalid");
+        }
+        this.value = value;
+    }
+
+    public static Email of(String value) {
+        return new Email(value);
+    }
+
+    private boolean validate(String value) {
+        return Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email1 = (Email) o;
+        return Objects.equals(value, email1.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
