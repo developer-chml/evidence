9a2bd282ab3d0cf695e1a7c1ed1840a446d5a497
CHML <developerchml@gmail.com>
2025-04-23 17:32:38 -0300

    Implementa UserController

--- src/main/java/com/github/developerchml/evdbackend/infrastruct/controllers/UserController.java
+++ src/main/java/com/github/developerchml/evdbackend/infrastruct/controllers/UserController.java
@@ -1,16 +1,59 @@
 package com.github.developerchml.evdbackend.infrastruct.controllers;
 
+import com.github.developerchml.evdbackend.core.services.CRUDService;
 import com.github.developerchml.evdbackend.core.services.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.developerchml.evdbackend.infrastruct.requests.RequestUserDTO;
+import com.github.developerchml.evdbackend.infrastruct.responses.ResponseUserDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/users/v1")
-public class UserController {
-    private final UserService userService;
+@RequestMapping("/api/v1/users")
+public class UserController implements CRUDController<RequestUserDTO, ResponseUserDTO, Long> {
+    private final CRUDService<RequestUserDTO, ResponseUserDTO, Long> userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/{value}")
+    @Override
+    public ResponseEntity<ResponseUserDTO> findById(@PathVariable Long value) {
+        return ResponseEntity.ok(userService.findById(value));
+    }
+
+    @GetMapping
+    @Override
+    public ResponseEntity<List<ResponseUserDTO>> listAll() {
+        return ResponseEntity.ok(userService.listAll());
+    }
+
+    @PostMapping
+    @Override
+    public ResponseEntity<ResponseUserDTO> save(@RequestBody RequestUserDTO dto) {
+        return ResponseEntity.ok(userService.save(dto));
+    }
+
+    @PutMapping("/{value}")
+    @Override
+    public ResponseEntity<ResponseUserDTO> update(@PathVariable Long value, @RequestBody RequestUserDTO dto) {
+        return ResponseEntity.ok(userService.update(value, dto));
+    }
+
+    @DeleteMapping("/{value}")
+    @Override
+    public ResponseEntity<Void> softDelete(@PathVariable Long value) {
+        userService.softDelete(value);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/force-delete/{value}")
+    @Override
+    public ResponseEntity<Void> forceDelete(@PathVariable Long value) {
+        userService.forceDelete(value);
+        return ResponseEntity.noContent().build();
     }
 
 }
