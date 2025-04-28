b7034d1247185ccfb17589fbaf914bf5edc11650
CHML <developerchml@gmail.com>
2025-04-28 14:53:47 -0300

    Implementa o metodo findByTitleAndOccurredAtAndOperation
    Para facilitar a validação de registro existente.

--- src/main/java/com/github/developerchml/evdbackend/core/repositories/OccurrenceRepository.java
+++ src/main/java/com/github/developerchml/evdbackend/core/repositories/OccurrenceRepository.java
@@ -1,7 +1,13 @@
 package com.github.developerchml.evdbackend.core.repositories;
 
 import com.github.developerchml.evdbackend.core.entities.occurrence.Occurrence;
+import com.github.developerchml.evdbackend.core.entities.occurrence.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface OccurrenceRepository extends JpaRepository<Occurrence, Long> {
+
+    Optional<Occurrence> findByTitleAndOccurredAtAndOperation(String title, LocalDate occurredAt, Operation operation);
 }
